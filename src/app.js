48ffda000b4ceb411346773e034aa189f1933ae3
Quality <56773210+easternpromises@users.noreply.github.com>
2020-08-17 15:30:10 -0700

    Get buttons with const
    Get the buttons first.  Then do forEach() loop and add click event.

---
+++ src/app.js
@@ -0,0 +1,10 @@
+// Using selectors inside the element.
+// Traversing the DOM.
+
+const btns = document.querySelectorAll('.question-btn'); // Get all the buttons from HTML. Then loop forEach().
+
+btns.forEach(function(buttons){
+    buttons.addEventListener('click', function(e){
+        console.log(e.currentTarget);
+    })
+})
