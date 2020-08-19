963e4ea8a7f7d488609aaa4999fa407552e30876
Quality <56773210+easternpromises@users.noreply.github.com>
2020-08-18 19:25:04 -0700

    Formatted using prettier
    Formatted using prettier

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 // Using selectors inside the element.
 // Traversing the DOM.
 
-const btns = document.querySelectorAll('.question-btn'); // Get all the buttons from HTML. Then loop forEach().
+const btns = document.querySelectorAll(".question-btn"); // Get all the buttons from HTML. Then loop forEach().
 
-btns.forEach(function(buttons){
-    buttons.addEventListener('click', function(e){
-        const question = e.currentTarget.parentElement.parentElement;
-        question.classList.toggle('show-text');
-    });
+btns.forEach(function (buttons) {
+  buttons.addEventListener("click", function (e) {
+    const question = e.currentTarget.parentElement.parentElement;
+    question.classList.toggle("show-text");
+  });
 });
